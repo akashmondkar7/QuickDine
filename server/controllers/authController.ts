7c17094ff305335057e07922984de47aa84db872
akashmondkar7 <akashmondkar03@gmail.com>
2026-07-23 21:52:48 +0530

    save

---
+++ server/controllers/authController.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+// Register a new user
+// POST /api/auth/register
+
+
+
+export const registerUser =async (req:Request,res :Response): Promise<void> => {
+    
+}
