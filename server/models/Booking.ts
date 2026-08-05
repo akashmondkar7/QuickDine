524ff9e92932f79e6a0491585c971b40032f1cbc
akashmondkar7 <akashmondkar03@gmail.com>
2026-08-05 22:00:14 +0530

    booking model

---
+++ server/models/Booking.ts
@@ -0,0 +1,43 @@
+import { Document, model, Schema , Types} from "mongoose";
+
+export interface IBooking extends Document{
+   user:Types.ObjectId;
+    restaurant:Types.ObjectId;
+    date:Date;
+    time:string;
+    guests:number;
+    occasion?:string;
+    specialRequests?:string;
+    status:"confirmed"|"cancelled" |"completed";
+    bookingId:string;
+    createdAt:Date;
+    updatedAT:Date;
+
+}
+
+const UserSchema = new Schema<IUser>(
+    {
+      name:{type:String,required:true},
+      email:{type:String,required:true,unique:true,trim:true ,lowercase:true},
+      password:{type:String,required:true,minlength:6},
+      phone:{type:String,required:true,minlength:10},
+      role:{type:String,enum:["user","admin","owner"],default:"user"},
+
+
+    },
+    {
+        timestamps:true
+    }
+   
+
+)
+// Remove Password when converting to JSON
+
+UserSchema.set("toJSON",{
+    transform:(doc ,ret)=>{
+      delete ret.password;
+      return ret
+    }
+})
+
+export const User =model<IUser>("User",UserSchema)
